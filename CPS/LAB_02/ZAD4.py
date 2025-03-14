f4daff0c6bc078344458b49cf2fbfd6c83654f6b
fifol <fkucharski@student.agh.edu.pl>
2025-03-14 15:41:18 +0100

    finished 1,2,3,4

--- CPS/LAB_02/ZAD4.py
+++ CPS/LAB_02/ZAD4.py
@@ -1,47 +1,37 @@
-
-from scipy.io import wavfile
-import matplotlib.pyplot as plt
 import numpy as np
-import ZAD1
+import matplotlib.pyplot as plt
+import scipy.io.wavfile as wav
 from CPS.LAB_02.ZAD1 import DCT_matrix
 
+fs, samples = wav.read("mowa.wav")
 
-f, samples = wavfile.read("mowa.wav")
-
-
+# wyswietlanie nagrania
 plt.plot(np.arange(len(samples)), samples)
-
-plt.title("Waveform of Audio Signal")
 plt.show()
 
 M = 10
 N = 256
-fragments=[]
-A = DCT_matrix(N)
+fragments = []
 
-begin_ = np.linspace(0,len(samples)-N,M,dtype=int)
-for n1 in begin_:
+samples_found= np.linspace(0,len(samples) - N,M,dtype=int)
+for n1 in samples_found:
     n2 = n1 + N
     fragments.append(samples[n1:n2])
 
-
-plt.figure(figsize=(20,2*M))
+print(fragments)
+plt.figure(figsize=(17,17))
 for k in range(M):
     x_k = fragments[k]
-    y_k = A @ x_k
-    freqs = np.linspace(0, f / 2, N)
+    y_k = DCT_matrix(N) @ x_k
+    freqs = np.linspace(0, fs, N)
 
-    plt.subplot(M,2 ,2*k+1)
-    plt.plot(np.arange(N),x_k)
-    plt.xlabel("Sample")
-    plt.ylabel("Amplitude")
+    plt.subplot(M,2,2*k+1)
+    plt.plot(np.arange(N), x_k)
 
-
-    plt.subplot(M,2 ,2*k+2)
-    plt.plot(freqs,y_k)
-    plt.xlabel("Frequency")
-    plt.ylabel("Amplitude")
-
-
+    plt.subplot(M,2,2*k+2)
+    plt.plot(freqs, y_k)
 
 plt.show()
+
+
+
